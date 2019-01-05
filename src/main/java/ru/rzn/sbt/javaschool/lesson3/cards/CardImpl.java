7ae733b3b2ee16595e26fefb5e256cc0d362b4ae
dimaon <dimaon@gmail.com>
2019-01-05 17:15:45 +0300

    lesson 3 Final

--- src/main/java/ru/rzn/sbt/javaschool/lesson3/cards/CardImpl.java
+++ src/main/java/ru/rzn/sbt/javaschool/lesson3/cards/CardImpl.java
@@ -1,4 +1,19 @@
 package ru.rzn.sbt.javaschool.lesson3.cards;
 
-public class CardImpl {
+public class CardImpl implements Card {
+
+    public enum enumSuit{бубны, трефы, червы, пики}
+    public enum enumRank{туз, шестёрка, семёрка, восьмёрка, девятка, десятка, валет, дама, король}
+    private int suit;
+    private int rank;
+
+    public CardImpl(int suit, int rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return enumRank.values()[rank] + " " + enumSuit.values()[suit];
+    }
 }
--- src/main/java/ru/rzn/sbt/javaschool/lesson3/decrypt/Solution.java
+++ src/main/java/ru/rzn/sbt/javaschool/lesson3/decrypt/Solution.java
@@ -3,18 +3,40 @@ package ru.rzn.sbt.javaschool.lesson3.decrypt;
 public class Solution {
     /**
      * Дешфрование строки
-     *
+     * <p>
      * Реализуйте метод decrypt, осуществляющий расшифровку сроки encrypted с использованием {@link StringBuilder}.
-     *
+     * <p>
      * Входящая строка зашифрована следующим образом:
      * а) Зашифрованы только символы латинского алфавита, прочие следует оставлять в исходном виде
      * б) Каждый символ зашифрован ASCII кодом со смещением 9, например 'a' -> 'j'
      * в) После символа 'z' шифрование осуществляется циклически, независимо от регистра символа, например,
      * 'x' -> 'g', 'V' -> 'E'
-     *
+     * <p>
      * Не забудте проверку на null.
      */
-    public static String decrypt(String encrypted){
-        return null;
+    public static String decrypt(String encrypted) {
+        int kode = 9;
+        if (encrypted == null) return null;
+        StringBuilder encStr = new StringBuilder(encrypted);
+        StringBuilder decrypted = new StringBuilder();
+
+        for (int i = 0; i < encStr.length(); i++) {
+            char letter = encStr.charAt(i);
+            if(letter >= 'A' && letter <= 'Z')
+            {
+                letter -= kode;
+                if(letter < 'A')
+                    letter += 26;
+            }
+            else if(letter >= 'a' && letter <= 'z')
+            {
+                letter -= kode;
+                if(letter < 'a')
+                    letter += 26;
+            }
+
+            decrypted.append(letter);
+        }
+        return decrypted.toString();
     }
 }
